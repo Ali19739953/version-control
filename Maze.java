4c9957f77bbd96c6d4b4a6e6f6de2ffcddae28b1
Ali <ali69571@gmail.com>
2022-02-14 22:47:56 +0400

    Changed Messages 3-5 in Maze.java

--- Maze.java
+++ Maze.java
@@ -103,7 +103,7 @@ public class Maze
         
         if(!solved)
         {
-            System.out.println("MESSAGE 5"); // you have failed to escape.Future archeologists gaze upon your remains in bafflement.
+            System.out.println("MESSAGE 5"); // You have failed to escape.Future archeologists gaze upon your remains in bafflement.
         }
         
        GridViewer.view(grid);
